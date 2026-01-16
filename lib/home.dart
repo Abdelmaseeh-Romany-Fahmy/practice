d7596094bdc406df3b141328a1b66e139bd3b91d
Abdelmaseeh-Romany-Fahmy <apdelmaseehromany@gmail.com>
2026-01-16 21:40:45 +0200

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:practice/register.dart';
+
+class Home extends StatelessWidget {
+  static String routName ="home";
+
+   Home({super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+   ModelController modelController= ModalRoute.of(context)!.settings.arguments as ModelController;
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("name =>${modelController.name} "),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Email =>${modelController.Email}"),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:practice/home.dart';
+
+class Register extends StatelessWidget {
+  TextEditingController nameController =TextEditingController();
+  TextEditingController EmailController =TextEditingController();
+
+  static String routName ="Register";
+   Register({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+         title: Text("practice"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller:nameController ,
+
+              decoration:
+
+              InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue)
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red)
+                ),
+
+                hintText: "Name",
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                 label: Text("Name")
+              ),
+            ),
+
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: EmailController,
+              decoration:
+
+              InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red)
+                  ),
+
+                  hintText: "Email",
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                  label: Text("Email")
+              ),
+            ),
+
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(onPressed: (){
+Navigator.of(context).pushNamed(Home.routName,arguments:ModelController(name: nameController.text,Email: EmailController.text ) );
+            }, child: Text("add")),
+          )
+
+        ],
+      ),
+    );
+  }
+}
+class ModelController{
+  String name;
+  String Email;
+
+  ModelController({required this.name, required this.Email});
+
+
+}
